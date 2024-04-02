18dad2b5dad3b94736edc106e3714ebf03f14c6a
Priyanshu Dhoke <dhoke.priyanshu2002@gmail.com>
2024-04-02 06:40:09 +0530

    removed hosting

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -9,7 +9,7 @@ const MainContainer = () => {
 
     if(movies === null) return; //EARLY RETURN
 
-    const mainMovie = movies[8];
+    const mainMovie = movies[9];
 
     const {original_title, overview, id} = mainMovie;
 
