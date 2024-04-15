91b8c238a6fe93f8aaa83241a1b41a1323aa161f
Priyanshu Dhoke <dhoke.priyanshu2002@gmail.com>
2024-04-15 16:54:04 +0530

    strong password

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -4,8 +4,8 @@ export const checkValidData = (email, password) => {
     const isEmailValid = /[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
     const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
-    if(!isEmailValid) return "Email Id is not valid";
-    if(!isPasswordValid) return "Password is not valid";
+    if(!isEmailValid) return "Email Id is not valid.";
+    if(!isPasswordValid) return "Make sure the password is at least 8 characters including a number and a Uppercase letter.";
 
     return null;
 
